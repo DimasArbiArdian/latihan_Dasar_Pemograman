4cf7cf9f1e68b4bd7524ac824ff460e7bb4eb107
Dimas Arbi Ardian <114743682+DimasArbiArdian@users.noreply.github.com>
2022-11-02 20:49:29 +0700

    Tugas Dasar Pemrograman Sesi 6 (Dimas Arbi Ardian).py
    Tugas Dasar Pemrograman Sesi 6 membuat program untuk menentukan nilai kelulusan

---
+++ Tugas Dasar Pemrograman Sesi 6 (Dimas Arbi Ardian).py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+print("===== Program Nilai Kelulusan =====")
+nilai = int(input("Inputkan nilai akhir: "))
+if (nilai >= 90) and (nilai <=100):
+ grade = "A (Dengan Pujian)"
+elif (nilai >= 80) and (nilai <=89):
+ grade = "B (Sangat Memuaskan)"
+elif (nilai >= 70) and (nilai <=79):
+ grade = "C (Memuaskan)"
+elif (nilai >= 60) and (nilai <=69):
+ grade = "D (Tidak Memuaskan)"
+elif (nilai >= 0) and (nilai <=59):
+ grade = "E (Tidak LULUS)"
+else:
+    while nilai % 3 != 1 or nilai > 100:
+        nilai = int(input('Masukkan lagi: '))
+    print ("Benar")
+
+print ("Hasil akhir","Grade Anda: ", grade)
+
+
+# In[ ]:
+
+
+print("===== Program Nilai Kelulusan =====")
+while True:
+    try:
+        nilai = int(input("Inputkan nilai akhir: "))
+    except ValueError:
+        print("Inputan harus berupa number")
+    else:
+        if (nilai >= 90) and (nilai <=100):
+            print("")
+            print("A")
+            print("Dengan Pujian")
+        elif (nilai >= 80) and (nilai <=89):
+            print("")
+            print("B")
+            print("Sangat Memuaskan")
+        elif (nilai >= 70) and (nilai <=79):
+            print("")
+            print("C")
+            print("Memuaskan")
+        elif (nilai >= 60) and (nilai <=69):
+            print("")
+            print("D")
+            print("Tidak Memuaskan")
+        elif (nilai >= 0) and (nilai <=59):
+            print("")
+            print("E")
+            print("Tidak LULUS")
+        else:
+            "Invalid Input"
+
+
+# In[ ]:
+
+
+
+
